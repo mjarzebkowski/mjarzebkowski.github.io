6109a2310a8926f9cf28cf735672119689a63396
Unknown <yries.warrior@gmail.com>
2017-09-18 03:28:37 +0200

    Add Webpack
    Added node.js, add webpack config fixed routeind between files

--- js/main.js
+++ js/main.js
@@ -1,24 +1,7 @@
-<nav class="navbar navbar-inverse">
-<div class="container-fluid">
-    <div class="navbar-header">
-       <a class="navbar-brand" href="#">Maciej Jarzębkowski</a>
-    </div>
-    <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">Page 1</a></li>
-        <li><a href="#">Page 2</a></li>
-        <li><a href="#">Page 3</a></li>
 
-        <button type="button" class="btn">Basic</button>
-            <button type="button" class="btn btn-default">Default</button>
-            <button type="button" class="btn btn-primary">Primary</button>
-            <button type="button" class="btn btn-success">Success</button>
-            <button type="button" class="btn btn-info">Info</button>
-            <button type="button" class="btn btn-warning">Warning</button>
-            <button type="button" class="btn btn-danger">Danger</button>
-            <button type="button" class="btn btn-link">Link</button> 
-
-
-    </ul>
-</div>
-</nav>
+// Bootstrap core JavaScript
+require('./jquery-3.2.1.slim.min.js');
+require('./popper.min.js')
+require('./bootstrap.min.js')
+// IE10 viewport hack for Surface/desktop Windows 8 bug
+require('./ie10-viewport-bug-workaround.js');
