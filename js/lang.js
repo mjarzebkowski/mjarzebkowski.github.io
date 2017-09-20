773c5847e16b9b835d9d37ee7907df5f14fab3df
Unknown <yries.warrior@gmail.com>
2017-09-20 04:02:59 +0200

    in lang fixing

---
+++ js/lang.js
@@ -0,0 +1,179 @@
+// function cleanAll(){
+//     document.getElementById("aboutme").style.display="none";
+//     document.getElementById("frontend").style.display="none";
+//     document.getElementById("backend").style.display="none";
+//     document.getElementById("electronics").style.display="none";
+//     document.getElementById("administration").style.display="none";
+//     document.getElementById("hacking").style.display="none";
+// }
+
+// function setAboutMe(){
+//     cleanAll();
+//     document.getElementById("aboutme").style.display="initial";
+// }
+
+// function setFrontEnd(){
+//     document.getElementById("frontend").style.display="none";
+// }
+
+// function setBackEnd(){
+//     cleanAll();   
+//     document.getElementById("backend").style.display="none";
+// }
+
+// function setAdministration(){
+//     cleanAll();   
+//     document.getElementById("administration").style.display="none";
+// }
+
+// function setElectronics(){
+//     cleanAll();  
+//     document.getElementById("electronics").style.display="none";
+// }
+
+// function setHacking(){
+//     cleanAll();
+//     document.getElementById("hacking").style.display="none";
+// }
+
+// function setLangPL(){
+//     document.getElementById("pl").style.display="none";
+//     document.getElementById("en").style.display="none";
+// }
+
+// function setLangEN(){
+//     document.getElementById("pl").style.display="none";
+//     document.getElementById("en").style.display="none";
+// }
+
+
+
+// changeLang(lang)
+// {
+//     if(lang == "EN"){
+//         currentLang = "EN";
+//         document.getElementById()
+//     }
+
+//     if(lang == "PL"){
+//         currentLang = "PL";
+
+//     }
+
+// }
+
+// $('#someDiv').show();
+// $('#someDiv').hide();
+
+function cleanAll(){
+
+    $('.aboutme').hide();
+    $('.frontend').hide();
+    $('.backend').hide();
+    $('.electronics').hide();
+    $('.administration').hide();
+    $('.hacking').hide();
+    // $('#someDiv').hide();
+
+
+    // document.getElementById("aboutme").
+    // document.getElementById("aboutme").style.display="none";
+    // document.getElementById("frontend").style.display="none";
+    // document.getElementById("backend").style.display="none";
+    // document.getElementById("electronics").style.display="none";
+    // document.getElementById("administration").style.display="none";
+    // document.getElementById("hacking").style.display="none";
+}
+
+function setAboutMe(){
+    cleanAll();
+    $('.aboutme').show();
+    // document.getElementById("aboutme").style.display="initial";
+}
+
+function setFrontEnd(){
+    document.getElementById("frontend").style.display="none";
+}
+
+function setBackEnd(){
+    cleanAll();   
+    document.getElementById("backend").style.display="none";
+}
+
+function setAdministration(){
+    cleanAll();   
+    document.getElementById("administration").style.display="none";
+}
+
+function setElectronics(){
+    cleanAll();  
+    document.getElementById("electronics").style.display="none";
+}
+
+function setHacking(){
+    cleanAll();
+    document.getElementById("hacking").style.display="none";
+}
+
+function setLangPL(){
+    document.getElementById("pl").style.display="none";
+    document.getElementById("en").style.display="none";
+}
+
+function setLangEN(){
+    document.getElementById("pl").style.display="none";
+    document.getElementById("en").style.display="none";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    function cleanAll(){
+        document.getElementById("aboutme").style.display="none";
+        document.getElementById("frontend").style.display="none";
+        document.getElementById("backend").style.display="none";
+        document.getElementById("electronics").style.display="none";
+        document.getElementById("administration").style.display="none";
+        document.getElementById("hacking").style.display="none";
+    }
+    
+    function setAboutMe(){
+        cleanAll();
+        document.getElementById("aboutme").style.display="initial";
+    }
+    
+    function setFrontEnd(){
+        document.getElementById("frontend").style.display="none";
+    }
+    
+    function setBackEnd(){
+        cleanAll();   
+        document.getElementById("backend").style.display="none";
+    }
+    
+    function setAdministration(){
+        cleanAll();   
+        document.getElementById("administration").style.display="none";
+    }
+    
+    function setElectronics(){
+        cleanAll();  
+        document.getElementById("electronics").style.display="none";
+    }
+    
+    function setHacking(){
+        cleanAll();
+        document.getElementById("hacking").style.display="none";
+    }
+    
+    function setLangPL(){
+        document.getElementById("pl").style.display="none";
+        document.getElementById("en").style.display="none";
+    }
+    
+    function setLangEN(){
+        document.getElementById("pl").style.display="none";
+        document.getElementById("en").style.display="none";
+    }
+    
+    
+});
--- js/main.js
+++ js/main.js
@@ -5,3 +5,6 @@ require('./popper.min.js')
 require('./bootstrap.min.js')
 // IE10 viewport hack for Surface/desktop Windows 8 bug
 require('./ie10-viewport-bug-workaround.js');
+// require('./EN_PL_langChange.js');
+// require('./PL_EN_langChange.js');
+require('./lang.js');
